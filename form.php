4692de63dc7bf74e092d503cf32b2c2c6447df06
RudeevK161 <90448797+RudeevK161@users.noreply.github.com>
2022-05-08 13:17:22 +0300

    Update form.php

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Контактная форма</title>
+    <title>РљРѕРЅС‚Р°РєС‚РЅР°СЏ С„РѕСЂРјР°</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -13,62 +13,62 @@
   
   if (!empty($messages)) {
     print('<div class="messages">');
-    // Выводим все сообщения.
+    // Р’С‹РІРѕРґРёРј РІСЃРµ СЃРѕРѕР±С‰РµРЅРёСЏ.
     foreach ($messages as $message) {
       print($message);
     }
     print('</div>');
   }
-  // Далее выводим форму отмечая элементы с ошибками классом error
-  // и задавая начальные значения элементов ранее сохраненными.
+  // Р”Р°Р»РµРµ РІС‹РІРѕРґРёРј С„РѕСЂРјСѓ РѕС‚РјРµС‡Р°СЏ СЌР»РµРјРµРЅС‚С‹ СЃ РѕС€РёР±РєР°РјРё РєР»Р°СЃСЃРѕРј error
+  // Рё Р·Р°РґР°РІР°СЏ РЅР°С‡Р°Р»СЊРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ СЌР»РµРјРµРЅС‚РѕРІ СЂР°РЅРµРµ СЃРѕС…СЂР°РЅРµРЅРЅС‹РјРё.
   ?>
 
   <div class="container">
     <h2>
-        Контактная форма
+        РљРѕРЅС‚Р°РєС‚РЅР°СЏ С„РѕСЂРјР°
     </h2>
     <form action="" method="POST">
-      Имя:<br><input type="text" name="name" <?php if ($errors['name']) {print 'class="group error"';} else print 'class="group"'; ?> value="<?php print $values['name']; ?>">
+      РРјСЏ:<br><input type="text" name="name" <?php if ($errors['name']) {print 'class="group error"';} else print 'class="group"'; ?> value="<?php print $values['name']; ?>">
       <br>
       E-mail:<br><input type="text" name="email" <?php if ($errors['email']) {print 'class="group error';} else print 'class="group"'; ?> value="<?php print $values['email']; ?>">
       <br>
       <div class="form-group">
-<legend for="year"class="group" style="color: white;">Дата рождения:</legend>
+<legend for="year"class="group" style="color: white;">Р”Р°С‚Р° СЂРѕР¶РґРµРЅРёСЏ:</legend>
 <input type="date" id="year" size="3" name="year" <?php if ($errors['year']) {print 'class="group error"';} else print 'class="group"';?> value="<?php print $values['year']; ?>">
 </div>
       <div <?php if ($errors['gender']) {print 'class="error"';} ?>>
-        Пол:<br>
-        <input class="radio" type="radio" name="gender" value="M" <?php if ($values['gender'] == 'M') {print 'checked';} ?>> Мужской
-        <input class="radio" type="radio" name="gender" value="W" <?php if ($values['gender'] == 'W') {print 'checked';} ?>> Женский
+        РџРѕР»:<br>
+        <input class="radio" type="radio" name="gender" value="M" <?php if ($values['gender'] == 'M') {print 'checked';} ?>> РњСѓР¶СЃРєРѕР№
+        <input class="radio" type="radio" name="gender" value="W" <?php if ($values['gender'] == 'W') {print 'checked';} ?>> Р–РµРЅСЃРєРёР№
       </div>
       <div <?php if ($errors['limbs']) {print 'class="error"';} ?>>
-        Количество конечностей:<br>
+        РљРѕР»РёС‡РµСЃС‚РІРѕ РєРѕРЅРµС‡РЅРѕСЃС‚РµР№:<br>
         <input class="radio" type="radio" name="limbs" value="4" <?php if ($values['limbs'] == '4') {print 'checked';} ?>> 4
         <input class="radio" type="radio" name="limbs" value="3" <?php if ($values['limbs'] == '3') {print 'checked';} ?>> 3
         <input class="radio" type="radio" name="limbs" value="2" <?php if ($values['limbs'] == '2') {print 'checked';} ?>> 2
         <input class="radio" type="radio" name="limbs" value="1" <?php if ($values['limbs'] == '1') {print 'checked';} ?>> 1
         <input class="radio" type="radio" name="limbs" value="0" <?php if ($values['limbs'] == '0') {print 'checked';} ?>> 0 
       </div>
-      Cверхспособности:<br>
+      CРІРµСЂС…СЃРїРѕСЃРѕР±РЅРѕСЃС‚Рё:<br>
       <select class="group" name="ability[]" size="3" multiple>
-          <option value="Бессмертие" <?php if (in_array("Бессмертие", $values['ability'])) {print 'selected';} ?>>Бессмертие</option>
-          <option value="Прохождение сквозь стены" <?php if (in_array("Прохождение сквозь стены", $values['ability'])) {print 'selected';} ?>>Прохождение сквозь стены</option>
-          <option value="Левитация" <?php if (in_array("Левитация", $values['ability'])) {print 'selected';} ?>>Левитация</option>
+          <option value="Р‘РµСЃСЃРјРµСЂС‚РёРµ" <?php if (in_array("Р‘РµСЃСЃРјРµСЂС‚РёРµ", $values['ability'])) {print 'selected';} ?>>Р‘РµСЃСЃРјРµСЂС‚РёРµ</option>
+          <option value="РџСЂРѕС…РѕР¶РґРµРЅРёРµ СЃРєРІРѕР·СЊ СЃС‚РµРЅС‹" <?php if (in_array("РџСЂРѕС…РѕР¶РґРµРЅРёРµ СЃРєРІРѕР·СЊ СЃС‚РµРЅС‹", $values['ability'])) {print 'selected';} ?>>РџСЂРѕС…РѕР¶РґРµРЅРёРµ СЃРєРІРѕР·СЊ СЃС‚РµРЅС‹</option>
+          <option value="Р›РµРІРёС‚Р°С†РёСЏ" <?php if (in_array("Р›РµРІРёС‚Р°С†РёСЏ", $values['ability'])) {print 'selected';} ?>>Р›РµРІРёС‚Р°С†РёСЏ</option>
       </select>
       <br>
-      Биография:<br><textarea class="group" name="bio" rows="3" cols="30"><?php print $values['bio']; ?></textarea>
+      Р‘РёРѕРіСЂР°С„РёСЏ:<br><textarea class="group" name="bio" rows="3" cols="30"><?php print $values['bio']; ?></textarea>
       <div  <?php if ($errors['checkbox']) {print 'class="error"';} ?>>
-        <input type="checkbox" name="checkbox" <?php if ($values['checkbox']) {print 'checked';} ?>> С контрактом ознакомлен(a) 
+        <input type="checkbox" name="checkbox" <?php if ($values['checkbox']) {print 'checked';} ?>> РЎ РєРѕРЅС‚СЂР°РєС‚РѕРј РѕР·РЅР°РєРѕРјР»РµРЅ(a) 
       </div>
-      <input type="submit" id="send" value="ОТПРАВИТЬ">
+      <input type="submit" id="send" value="РћРўРџР РђР’РРўР¬">
     </form>
   </div>
   <div class="container">
     <?php
       if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login']))
-        print('<a href="login.php" class = "enter-exit" title = "Log out">Выйти</a>');
+        print('<a href="login.php" class = "enter-exit" title = "Log out">Р’С‹Р№С‚Рё</a>');
       else
-        print('<a href="login.php" class = "enter-exit"  title = "Log in">Войти</a>');
+        print('<a href="login.php" class = "enter-exit"  title = "Log in">Р’РѕР№С‚Рё</a>');
     ?>
   </div>
 </body>
